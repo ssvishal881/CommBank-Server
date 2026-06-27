95ad5933f257f23297e8f8ed1c5641858adf2a71
varshaj739 <150647598+VarshaRJ1210@users.noreply.github.com>
2026-06-27 19:13:30 +0530

    Add goal icon suppourt and emoji picker

--- CommBank.Tests/GoalControllerTests.cs
+++ CommBank.Tests/GoalControllerTests.cs
@@ -16,7 +16,7 @@ public class GoalControllerTests
     }
 
     [Fact]
-    public async void GetAll()
+    public async Task GetAll()
     {
         // Arrange
         var goals = collections.GetGoals();
@@ -42,7 +42,7 @@ public class GoalControllerTests
     }
 
     [Fact]
-    public async void Get()
+    public async Task Get()
     {
         // Arrange
         var goals = collections.GetGoals();
@@ -63,12 +63,27 @@ public class GoalControllerTests
     }
 
     [Fact]
-    public async void GetForUser()
+    public async Task GetForUser()
     {
         // Arrange
-        
+        var goals = collections.GetGoals();
+        var users = collections.GetUsers();
+        IGoalsService goalsService = new FakeGoalsService(goals, goals[0]);
+        IUsersService usersService = new FakeUsersService(users, users[0]);
+        GoalController controller = new(goalsService, usersService);
+
         // Act
-        
+        var httpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext();
+        controller.ControllerContext.HttpContext = httpContext;
+        var result = await controller.GetForUser(users[0].Id!);
+
         // Assert
+        Assert.NotNull(result);
+        Assert.NotEmpty(result);
+
+        foreach (var goal in result)
+        {
+            Assert.IsType<Goal>(goal);
+        }
     }
 }
